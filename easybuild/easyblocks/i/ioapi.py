86d0d68adce51f4b1a0c64984952ce63d5ddf159
Robert Schmidt <rjeschmi@gmail.com>
2015-09-10 06:53:33 -0700

    adding a build step to ioapi

--- easybuild/easyblocks/i/ioapi.py
+++ easybuild/easyblocks/i/ioapi.py
@@ -17,4 +17,18 @@ class EB_ioapi(ConfigureMake):
         (out, _) = run_cmd( cmd, log_all=True, simple=False)
         return out
 
-    
+    def build_step(self, verbose=False, path=None):
+        """
+        Start the actual build
+        - typical: make -j X
+        """
+
+        paracmd = ''
+        if self.cfg['parallel']:
+            paracmd = "-j %s" % self.cfg['parallel']
+
+        cmd = "%s make all %s %s" % (self.cfg['prebuildopts'], paracmd, self.cfg['buildopts'])
+
+        (out, _) = run_cmd(cmd, path=path, log_all=True, simple=False, log_output=verbose)
+
+        return out    
